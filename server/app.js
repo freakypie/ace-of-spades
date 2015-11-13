ed649a5e84149188515f4355d9a88bc72b2049f6
Randall <randall@velocitywebworks.com>
2015-11-13 09:48:06 -0700

    Added bower components

--- server/app.js
+++ server/app.js
@@ -6,6 +6,7 @@ var reload = require("reload");
 
 server.listen(8005);
 
+app.use('/bower', express.static(__dirname + '/../bower_components'));
 app.use('/assets', express.static(__dirname + '/public'));
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
